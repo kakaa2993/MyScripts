7262c94d723f9d72d22040888e957249f133a96e
kakaa2993 <kakaa2993@gmail.com>
2024-03-16 01:05:15 +0100

    update the code, add delete recycle bin functionality

--- CleanPc/Clean.bat
+++ CleanPc/Clean.bat
@@ -1,4 +1,5 @@
 @echo off
+
 del/s/f/q c:\windows\temp\*.*
 rd/s/q c:windows\temp
 md c:\windows\temp
@@ -15,9 +16,18 @@ deltree/y c:\windows\cookies
 deltree/y c:\windows\recent
 deltree/y c:\windows\spool\printers
 del c:\WIN386.SWP
+
 cls
 
+:: Loop through all drive letters from C to Z
+for %%d in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+    rem Check if the drive exists
+    if exist "%%d:\$Recycle.Bin" (
+        rem Delete the recycle bin folder for the drive
+        echo Deleting recycle bin for drive %%d
+        rd /s /q "%%d:\$Recycle.Bin"
+    )
+)
 
-
-
+echo All recycle bins deleted.
 
